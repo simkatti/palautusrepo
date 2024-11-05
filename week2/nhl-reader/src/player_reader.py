edb1e0216c43746040c6d97aa95bcdda51cdf254
Simona <simonacallegari@outlook.com>
2024-11-05 23:16:53 +0200

    week2 submissions

---
+++ week2/nhl-reader/src/player_reader.py
@@ -0,0 +1,15 @@
+import requests
+from player import Player
+
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+
+
+    def read(self):
+        players = []
+        response = requests.get(self.url).json()
+        for player_dict in response:
+            player = Player(player_dict)
+            players.append(player)
+        return players
