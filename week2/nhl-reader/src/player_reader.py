4d773b0435e077255197ac6a625f94570d18f8f5
Simona <simonacallegari@outlook.com>
2024-11-05 23:43:09 +0200

    week 2 submissions

--- week2/nhl-reader/src/player_reader.py
+++ week2/nhl-reader/src/player_reader.py
@@ -5,10 +5,6 @@ class PlayerReader:
     def __init__(self, url):
         self.url = url
 
-<<<<<<< HEAD
-=======
-
->>>>>>> edb1e02 (week2 submissions)
     def read(self):
         players = []
         response = requests.get(self.url).json()
